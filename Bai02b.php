dfd03ec5a354a7ce46b73689ea68cce71a8ee988
PTrinh202 <118448037+PTrinh202@users.noreply.github.com>
2023-02-17 20:09:40 +0700

    Add files via upload

---
+++ Bai02b.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+    <table background = "../PICS/pngtree-simple-clean-hydrangea-flower-background-image_598394.jpg" border="1px" width = "550px" align="center">
+        <tbody>
+            <tr align = "center">
+                <td><img src = "../PICS/702fa9023118c16174956143da328bb8.jfif" width="150px"><br>Hoa 1</td>
+                <td><img src = "../PICS/anh-gio-hoa-sen-dep-y-nghia_091937096.jpg" width="150px"><br> Hoa 2</td>
+                <td><img src = "../PICS/7fbee7c6518c40efcd0bc722f592d77e.webp" width="150px"><br> Hoa 3</td> 
+                <td><h2>SẢN<br>PHẨM<br>GIỎ<br>HOA<br>KHUYẾN<br>MÃI</h2></td>
+                <td><img src = "../PICS/hinh-anh-lang-hoa-dep-3.jpg" width="150px"><br> Hoa 4</td>
+                <td><img src = "../PICS/hoa-cam-gio-dep(1).jpg" width="150px"><br> Hoa 5</td>
+                <td><img src = "../PICS/Fs1.1.jpg" width="150px"><br> Hoa 6</td>
+            </tr>
+        </tbody>
+    </table>
+    </body>
+</html>
