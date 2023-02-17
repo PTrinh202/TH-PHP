2f470484ae04f65aca858aaf87332c57ebb0d6f3
PTrinh202 <118448037+PTrinh202@users.noreply.github.com>
2023-02-17 08:55:42 +0700

    Add files via upload

---
+++ Bai01a.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            Introduction of Cats
+        </title>
+    </head>
+    <body bgcolor="yellow">
+       <h1>Các mèo của năm</h1>
+       <img src = "../PICS/34234234.jpg"> Hình 1
+       <img src = "../PICS/KNOWYOURMEME-sad-cat-crying-1120.jpg"> Hình 2
+       <img src = "../PICS/5954b408c66525ad932faa693a647e3f.jpg"> HÌnh 3
+    </body>
+</html>
---
+++ Bai01b.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            Introduction of Cats
+        </title>
+    </head>
+    <body bgcolor="yellow">
+       <h1>Các mèo của năm</h1>
+       <img src = "../PICS/34234234.jpg" width="120px"> Hình 1<br>
+       <img src = "../PICS/KNOWYOURMEME-sad-cat-crying-1120.jpg" width="120px"> Hình 2<br>
+       <img src = "../PICS/5954b408c66525ad932faa693a647e3f.jpg" width="120px"> Hình 3<br>
+    </body>
+</html>
