135b82a8310dca0aacaecdb406ac96b1dfb0e2d8
PTrinh202 <118448037+PTrinh202@users.noreply.github.com>
2023-02-17 08:57:13 +0700

    Add files via upload

---
+++ Bai02a.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+    <table background = "../PICS/pngtree-simple-clean-hydrangea-flower-background-image_598394.jpg" border="1px" width = "550px" align="center">
+        <thead>   
+            <tr>
+                <th colspan = "2">Sản phẩm giỏ hoa khuyến mãi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr align = "center">
+            <td><img src = "../PICS/702fa9023118c16174956143da328bb8.jfif" width="240px"><br>Hoa 1</td>
+            <td><img src = "../PICS/anh-gio-hoa-sen-dep-y-nghia_091937096.jpg" width="220px"><br> Hoa 2</td>
+            </tr>
+            <tr align = "center">
+            <td><img src = "../PICS/7fbee7c6518c40efcd0bc722f592d77e.webp" width="240px"><br> Hoa 3</td>
+            <td><img src = "../PICS/hinh-anh-lang-hoa-dep-3.jpg" width="290px"><br> Hoa 4</td>
+            </tr>
+        </tbody>
+    </table>
+    </body>
+</html>
